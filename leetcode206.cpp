f54fea637040ea11765f94c56c0e8353b715ca83
cnfyh <feiyuhang5@gmail.com>
2026-06-02 21:04:41 +0800

    leetcode206

--- leetcode206.cpp
+++ leetcode206.cpp
@@ -6,22 +6,18 @@
     ListNode(int x, ListNode *next) : val(x), next(next) {}
  };
 /*给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。*/
+/* 双指针写法 */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ListNode* cur=head;
         ListNode* prev=nullptr;
         while (cur) {
-            if (cur->next) {
-                ListNode*temp=cur->next;
-                cur->next=prev;
-                prev=cur;
-                cur=temp;
-            }else {
-                cur->next=prev;
-                break;
-            }
+            ListNode*temp=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=temp;
         }
-        return cur;
+        return prev;
     }
 };
